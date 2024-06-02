7b907a152940344d3552d7919438b2402137c40f
tejaswi-Boni <tejaswirajmohan@gmail.com>
2024-06-02 23:04:08 +0800

    Requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+   name = 'Generative AI Project',
+   version = '0.0.0',
+   author = 'tejaswi',
+   author_email='tejaswi@gmail.com',
+   packages=find_packages(),
+   install_requires=[]
+)
